ded6571d1fcfc434c41b7935d88eee53daea4692
Murilo <mug.fac@gmail.com>
2025-10-15 23:29:54 -0300

    Questão 8: Status dos livros (Disponível ou Emprestado)

---
+++ q8.sql
@@ -0,0 +1,7 @@
+SELECT l.titulo,
+       CASE 
+           WHEN e.data_devolucao IS NULL THEN 'Emprestado'
+           ELSE 'Disponível'
+       END AS Status
+FROM livros l
+LEFT JOIN emprestimos e ON l.livro_id = e.livro_id;
